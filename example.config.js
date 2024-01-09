87b5f9242361957f349ac4bc205453e2877303b8
Spamexwddwd <156115601+Spamexwddwd@users.noreply.github.com>
2024-01-09 18:13:23 +0100

    Update example.config.js

--- example.config.js
+++ example.config.js
@@ -1,15 +1,15 @@
 module.exports = {
     client: {
-        token: '', // ← Your bot token (.env IS RECOMMENDED)
-        id: '' // ← Your bot ID
+        token: 'MTE5NDMyNzIzNDM1NDM1NjMyNQ.GmtvHr.-uSCRywvJSUGxIKyDe7vOvpY8_RIuXI8LWT6Gs', // ← Your bot token (.env IS RECOMMENDED)
+        id: '1194327234354356325' // ← Your bot ID
     },
     modmail: {
-        guildId: '', // ← Your server ID
-        categoryId: '', // ← The modmail category ID
-        staffRoles: [''], // ← The modmail staff roles IDs
+        guildId: '1186055556918673420', // ← Your server ID
+        categoryId: '1194326842396639252', // ← The modmail category ID
+        staffRoles: ['1186055748631928863'], // ← The modmail staff roles IDs
         mentionStaffRolesOnNewMail: true // ← Mention staff roles when there is a new mail?
     },
     logs: {
-        webhookURL: '' // ← The logging webhook URL (OPTIONAL) (.env IS RECOMMENDED)
+        webhookURL: 'https://discord.com/api/webhooks/1194327030779629579/YH18p0-Y43LjGZTvlU-VvRDk60FC2K-1KKAAkpu5KxvNng0FFMIInCyHW0bLZbFvONWf' // ← The logging webhook URL (OPTIONAL) (.env IS RECOMMENDED)
     }
 };
